f97f2185a100d83349fcbf2438e9cb7b05ceafca
Igor Grosu <igrgro@gmail.com>
2023-05-21 13:23:28 +0300

    set up webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, './static/js'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  }
+};
